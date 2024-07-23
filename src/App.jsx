79cdaac589768b24aa76874294db42d91a8195ef
yassine <rezguiyassine224@gmail.com>
2024-07-23 21:30:40 +0100

    add modal when i resiz the window

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/Footer";
 
 function App() {
-  return <div className="container">
+  return( 
+  <div className="container">
     <Header/>
+    <div className="divider"/>
     <Hero/>
+    <div className="divider"/>
     <Main/>
+    <div className="divider"/>
     <Contact/>
+    <div className="divider"/>
     <Footer/>
   </div>
+)
   
 }
 
--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+
 import "./header.css";
 
 function Header() {
-  return <div>header</div>;
+  return <header className=" flex">
+    {/* for make the nav in the center */}
+    <div></div>
+
+
+    <button className="menu">show model</button>
+
+    <nav>
+    <ul className="flex">
+      <li><a href="">About</a></li>
+      <li><a href="">Articles</a></li>
+      <li><a href="">Projects</a></li>
+      <li><a href="">Speaking</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+    </nav>
+    <button>light</button>
+<div className="fixed">
+    <ul className="modal ">
+      <li><a href="">About</a></li>
+      <li><a href="">Articles</a></li>
+      <li><a href="">Projects</a></li>
+      <li><a href="">Speaking</a></li>
+      <li><a href="">uses</a></li>
+    </ul>
+
+</div>
+
+    </header>;
 }
 
 export default Header;
