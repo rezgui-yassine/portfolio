c383bc39ade20dce988db2cf511255e51fa6cc71
yassine <rezguiyassine224@gmail.com>
2024-07-24 11:28:34 +0100

    fixe the style of header and add the icon

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,36 +1,75 @@
-
+import { useState } from "react";
 import "./header.css";
 
 function Header() {
-  return <header className=" flex">
-    {/* for make the nav in the center */}
-    <div></div>
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <header className=" flex">
+      {/* for make the nav in the center */}
+      <div></div>
 
+      <button 
+        onClick={() => {
+          setShowModal(true);
+        }}
+        className="menu icon-menu"
+      >
+      
+      </button>
 
-    <button className="menu">show model</button>
-
-    <nav>
-    <ul className="flex">
-      <li><a href="">About</a></li>
-      <li><a href="">Articles</a></li>
-      <li><a href="">Projects</a></li>
-      <li><a href="">Speaking</a></li>
-      <li><a href="">Contact</a></li>
-    </ul>
-    </nav>
-    <button>light</button>
-<div className="fixed">
-    <ul className="modal ">
-      <li><a href="">About</a></li>
-      <li><a href="">Articles</a></li>
-      <li><a href="">Projects</a></li>
-      <li><a href="">Speaking</a></li>
-      <li><a href="">uses</a></li>
-    </ul>
-
-</div>
-
-    </header>;
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button >
+        <span className="icon-moon-o"></span>
+      </button>
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal ">
+            <li>
+              <button
+                className="icon-clear"
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              />
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Articles</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Speaking</a>
+            </li>
+            <li>
+              <a href="">uses</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default Header;
