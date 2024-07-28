0fc2fcb18072bfd6f635f972658e4b2f906d715e
yassine <rezguiyassine224@gmail.com>
2024-07-28 12:28:59 +0100

    fix the style of footer

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './footer.css'
 export default function Footer() {
   return (
-    <footer>
-      <ul>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Speaking</li>
-        <li>Uses</li>
+    <footer className='flex'>
+      <ul className='flex'>
+        <li><a href="">About</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Speaking</a></li>
+        <li><a href="">Uses</a></li>
       </ul>
       <p>&copy; 2024 Yassine Rezgui. All Rights Reserved.</p>
 
