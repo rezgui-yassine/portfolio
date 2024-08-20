19f7d13a1830f19bb025bf4d7d2f5c209b5ca970
Rezgui Yassine <rezguiyassine224@gmail.com>
2024-08-20 20:51:42 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Header from "./components/1-header/Header";
 import Hero from "./components/2-hero/Hero";
@@ -7,21 +7,37 @@ import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/Footer";
 
 function App() {
-  return( 
-  <div className="container">
-    <Header/>
-   
-    <Hero/>
-    <div className="divider"/>
-    <Main/>
-    <div className="divider"/>
-    <Contact/>
-    <div className="divider"/>
-    <Footer/>
-    <button className="icon-keyboard_arrow_up  scrole2top"></button>
-  </div>
-)
-  
+  useEffect(() => {
+    window.addEventListener(
+      "scroll",
+      () => {
+        if (window.scrollY > 300) {
+          setShowScroll(true);
+        } else {
+          setShowScroll(false);
+        }
+      },
+      { passive: true }
+    );
+  }, []);
+  const [showScroll, setShowScroll] = React.useState(false);
+
+  return (
+    <div className="container" id="up">
+      <Header />
+      <Hero />
+      <div className="divider" />
+      <Main />
+      <div className="divider" />
+      <Contact />
+      <div className="divider" />
+      <Footer />
+
+      <a href="#up" style={{ opacity: showScroll ? 1 : 0, transition: "2s" }}>
+        <button className="icon-keyboard_arrow_up  scrole2top"></button>
+      </a>
+    </div>
+  );
 }
 
 export default App;
--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./header.css";
 
 function Header() {
   const [showModal, setShowModal] = useState(false);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("currentMode") ?? "dark"
+  );
+  useEffect(() => {
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    }
+  }, [theme]);
   return (
     <header className=" flex">
-       <button 
+      <button
         onClick={() => {
-          setShowModal(true);
+          // send value to local soarge 
+          localStorage.setItem("currentMode",theme === "dark" ? "light" : "dark")
+          // get the value from the local soarge
+          setTheme(localStorage.getItem("currentMode"))
         }}
         className="menu icon-menu flex"
-      >
-      </button>
+      ></button>
       {/* for make the nav in the center */}
-      <div/>
-     
+      <div />
 
       <nav>
         <ul className="flex">
@@ -35,9 +48,27 @@ function Header() {
           </li>
         </ul>
       </nav>
-      <button className="mode flex" >
-        <span className="icon-moon-o"></span>
+
+      <button
+        onClick={() => {
+          // Send value to LS
+          localStorage.setItem(
+            "currentMode",
+            theme === "dark" ? "light" : "dark"
+          );
+
+          // get value from LS
+          setTheme(localStorage.getItem("currentMode"));
+        }}
+        className="mode flex"
+      >
+        {theme === "dark" ? (
+          <span className="icon-moon-o"> </span>
+        ) : (
+          <span className="icon-sun"> </span>
+        )}
       </button>
+
       {showModal && (
         <div className="fixed">
           <ul className="modal ">
